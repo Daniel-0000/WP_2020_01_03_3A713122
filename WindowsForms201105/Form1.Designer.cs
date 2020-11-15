b268e24d5f6387961be8047c88b61c5e30f64e16
Daniel <dog89070388@gmail.com>
2020-11-15 16:12:56 +0800

    加入專案檔案。

---
+++ WindowsForms201105/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WindowsForms201105
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(494, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(233, 84);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "心痛的感覺";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::WindowsForms201105.Properties.Resources.優質_201105_11;
+            this.pictureBox3.Location = new System.Drawing.Point(31, 156);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(416, 263);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.richTextBox1.Location = new System.Drawing.Point(494, 156);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(233, 262);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(65, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(356, 48);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "女神如何拒絕你";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsForms201105/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsForms201105.Properties;
+
+namespace WindowsForms201105
+{
+    public partial class Form1 : Form
+    {
+        List<Image> list = new List<Image>();
+        List<string> text1 = new List<string>();
+        
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int randomNum = new Random().Next(12);
+            pictureBox3.Image = list[randomNum];
+            richTextBox1.Text = text1[randomNum];
+        }
+
+        
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            list.Add(Resources.優質_201105_0);
+            list.Add(Resources.優質_201105_1);
+            list.Add(Resources.優質_201105_2);
+            list.Add(Resources.優質_201105_3);
+            list.Add(Resources.優質_201105_4);
+            list.Add(Resources.優質_201105_5);
+            list.Add(Resources.優質_201105_6);
+            list.Add(Resources.優質_201105_7);
+            list.Add(Resources.優質_201105_8);
+            list.Add(Resources.優質_201105_9);
+            list.Add(Resources.優質_201105_10);
+            list.Add(Resources.優質_201105_11);
+
+
+            text1.Add("我先去洗澡了");
+            text1.Add("你是好人");
+            text1.Add("我們還是可以當朋友");
+            text1.Add("你是我最好的朋友");
+            text1.Add("你人其實還不錯");
+            text1.Add("你跟我星座不合");
+            text1.Add("我媽說不喜歡你");
+            text1.Add("我家的狗討厭你");
+            text1.Add("媽祖昨天託夢跟我說的");
+            text1.Add("你人很好");
+            text1.Add("我們比較適合當朋友");
+            text1.Add("謝謝你的好意");
+        }
+    }
+}
